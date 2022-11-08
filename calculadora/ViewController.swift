66c7553a499f9bdbdab953955c3088ed4a033ece
anna.frias <anna.frias@col.vueling.com>
2022-11-08 14:17:13 +0100

    Initial Commit

---
+++ calculadora/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculadora
+//
+//  Created by annaF on 8/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
